38b9dde6c32c9a26fb252f9b5fbff1a7d3065483
boxofhope97 <hopejordangreenwood@gmail.com>
2023-11-23 19:06:24 +0000

    add untracked files

---
+++ pipeline.py
@@ -0,0 +1,13 @@
+import sys
+
+import pandas as pd
+
+print(sys.argv)
+
+day = sys.argv[1]
+
+# some pandas stuff
+
+# checking pandas imported successfully
+print(f'job finished successfully for day = {day}')
+
